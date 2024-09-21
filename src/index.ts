73964f9cf03f870242d1dab6ee28c38afd391b3c
Fozilbek Raimberdiyev <fl.raimberdiyev@gmail.com>
2024-09-22 00:35:10 +0500

    create(.npmignore)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-import "./laravel"
+#!/usr/bin/env node
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .command("make:model <name>")
+  .description("Create a new model")
+  .action((name) => {
+    console.log(name);
+  });
+program.parse(process.argv);
